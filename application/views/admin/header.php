4fb89d7c1d669591b1d4ff69fd7a11d163b75615
Sarabjit <sarabjit.xo@gmail.com>
2020-09-26 23:44:11 -0400

    designing changes

--- application/views/admin/header.php
+++ application/views/admin/header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>Bootstrap Example</title>
+  <title>Railway Reservation System</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
@@ -42,28 +42,30 @@
     }
   </style>
 </head>
-<body>
+<body class="appBody">
 
 <nav class="navbar navbar-inverse">
-  <div class="container-fluid">
+  <div class="container">
     <div class="navbar-header">
       <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>                        
       </button>
-      <a class="navbar-brand" href="">Logo</a>
+
+      <a class="navbar-brand" href="#">Railway Reservation System</a>
+
     </div>
     <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+      </ul>
       <ul class="nav navbar-nav">
     
         <li><a href="<?php echo base_url('admin/addtrain') ?>">Add trains</a></li>
         <li><a href="<?php echo base_url('admin/showtrains') ?>">Show Trains</a></li>
         <li><a href="<?php echo base_url('admin/showbooking') ?>">Show Bookings</a></li>
     
-      </ul>
-      <ul class="nav navbar-nav navbar-right">
-        <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
       </ul>
     </div>
   </div>
--- application/views/admin/login.php
+++ application/views/admin/login.php
@@ -5,28 +5,32 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <link rel="stylesheet" href="<?php echo base_url('assets/style.css');?>">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+  <style type="text/css">body {min-width: 100vw;min-height: 100vh;}</style>
 </head>
 <body>
-
-<div class="container">
-  <h2>Admin Login</h2>
-  <h2><?php echo $this->session->flashdata('invalid_login'); ?></h2> 
-  <form action="" method="post">
-    <div class="form-group">
-      <label for="email">Email:</label>
-      <input type="email" class="form-control" id="email" required placeholder="Enter email" name="email">
+<div class="login_outer">
+  <div class="login_inner">
+    <div class="container">
+      <h2>Admin Login</h2>
+      <h2><?php echo $this->session->flashdata('invalid_login'); ?></h2> 
+      <form action="" method="post">
+        <div class="form-group">
+          <label for="email">Email:</label>
+          <input type="email" class="form-control" id="email" required placeholder="Enter email" name="email">
+        </div>
+        <div class="form-group">
+          <label for="pwd">Password:</label>
+          <input type="password" class="form-control" id="pwd" required placeholder="Enter password" name="password">
+        </div>
+          
+        <button type="submit" class="btn btn-primary">Submit</button>
+      </form>
     </div>
-    <div class="form-group">
-      <label for="pwd">Password:</label>
-      <input type="password" class="form-control" id="pwd" required placeholder="Enter password" name="password">
-    </div>
-      
-    <button type="submit" class="btn btn-primary">Submit</button>
-  </form>
+  </div>
 </div>
-
 </body>
 </html>
